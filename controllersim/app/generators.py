ed1eaab77d7131199d74baa11d6e85379fe8d41c
David Schmid <schmid.david@outlook.de>
2022-07-10 18:27:12 +0200

    custom packages setup

--- controllersim/app/generators.py
+++ controllersim/app/generators.py
@@ -9,62 +9,80 @@ import signal
 import logging
 
 
-
 mqtt_broker_ip = config.get('MQTT', 'broker_ip')
 mqtt_broker_port = config.getint('MQTT', 'broker_port', fallback=1883)
 
 
 class Sensor():
-    def __init__(self, sample_interval, send_interval, mqtt_topic):
+    def __init__(self, name, sample_interval, send_interval, mqtt_topic):
+        self.name = name
         self.sampler = samplers.SineSampler(freq=1, noise=0.2)
-        self.process = mp.Process(target=self.run)
+        self.process = mp.Process(target=self._run, name=self.name)
         self.stop_event = mp.Event()
-        self.output = []
         self.sample_interval = sample_interval
         self.send_interval = send_interval
         self.mqtt_topic = mqtt_topic
+        logging.info("Sensor %s: spawned" % self.name)
 
-    def run(self):
+    def _run(self):
+        # Ignore SIGINT/keyboard interrupt on the subprocess
         signal.signal(signal.SIGINT, signal.SIG_IGN)
-        self.mqtt_client = mqtt.Client()
-        def foo():
-            logging.info('bar')
-        self.mqtt_client.on_connect_fail = foo()
+
+        # Setup MQTT Client
+        mqtt_client = mqtt.Client(protocol=mqtt.MQTTv5)
+        def on_connect_fail(client, userdata):
+            logging.warn('Sensor %s: MQTT Connection failed' % self.name)
+        def on_connect(client, userdata, flags, reasonCode, properties):
+            if reasonCode == 0:
+                logging.info('Sensor %s: MQTT connected' % self.name)
+        mqtt_client.on_connect = on_connect
+        mqtt_client.on_connect_fail = on_connect_fail
         try:
-            self.mqtt_client.connect(host=mqtt_broker_ip, port=mqtt_broker_port)
+            mqtt_client.connect(host=mqtt_broker_ip, port=mqtt_broker_port)
         except: pass
-        self.mqtt_client.loop_start()
+        mqtt_client.loop_start()
+
+        # Setup sample and send loops. 
+        # Asyncio loop runs sample and loop task until they're complete. 
+        # The loops only complete, when the stop_event is set. 
+        # Asyncio.gather is used to start the sleep at the same time as the coroutine, 
+        # other than runnning the coroutine and then sleep afterwards. 
+        output = []
+
+        async def sample_loop():
+            async def sample_coro():
+                time_stamp = time.time()
+                sample_value = self.sampler.sample(time_stamp)
+                sample = {'time': time_stamp, 'value': sample_value}
+                output.append(sample)
+                return
+            while not self.stop_event.is_set():
+                await asyncio.gather(asyncio.sleep(self.sample_interval), sample_coro())
+
+        async def send_loop():
+            async def send_coro():
+                payload = json.dumps(output)
+                output.clear()
+                mqtt_client.publish(topic=self.mqtt_topic, payload=payload)
+                return
+            while not self.stop_event.is_set():
+                await asyncio.gather(asyncio.sleep(self.send_interval), send_coro())
 
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        loop.run_until_complete(asyncio.gather(self.sample_coro(), self.send_coro()))
-        self.mqtt_client.loop_stop()
-
-    async def sample_coro(self):
-        async def sample_subcoro():
-            time_stamp = time.time()
-            sample_value = self.sampler.sample(time_stamp)
-            sample = {'time': time_stamp, 'value': sample_value}
-            self.output.append(sample)
-            return
-        while not self.stop_event.is_set():
-            await asyncio.gather(sample_subcoro(), asyncio.sleep(self.sample_interval))
-
-    async def send_coro(self):
-        async def send_subcoro():
-            payload = json.dumps(self.output)
-            self.output.clear()
-            self.mqtt_client.publish(topic=self.mqtt_topic, payload=payload)
-            return
-        while not self.stop_event.is_set():
-            await asyncio.gather(send_subcoro(), asyncio.sleep(self.send_interval))
+        loop.run_until_complete(asyncio.gather(sample_loop(), send_loop()))
+        mqtt_client.loop_stop()
+        logging.info("Sensor %s: stopped" % (self.name))
 
     def start(self):
+        self.stop_event.clear()
         self.process.start()
 
+    # Currently the stop function blocks until the send and sample loops have finished 
+    # the last iteration of the loop. A non-blocking implementation would be better. 
     def stop(self):
         self.stop_event.set()
-        self.process.join()
+        # logging.info("Sensor %s: stopped" % (self.name))
 
 if __name__ == '__main__':
     pass
--- controllersim/app/main.py
+++ controllersim/app/main.py
@@ -1,5 +1,6 @@
 # setup logging
 import logging
+import sys
 
 def warning_filter(record: logging.LogRecord):
     if record.levelno >= 30:
@@ -10,11 +11,11 @@ def warning_filter(record: logging.LogRecord):
 debug_formatter = logging.Formatter(
     '%(asctime)s %(levelname)-7s %(module)-20s %(message)s')
 
-stderr_handler = logging.StreamHandler()
+stderr_handler = logging.StreamHandler(sys.stderr)
 stderr_handler.setFormatter(debug_formatter)
 stderr_handler.setLevel(logging.WARNING)
 
-stdout_handler = logging.StreamHandler()
+stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setFormatter(debug_formatter)
 stdout_handler.setLevel(logging.DEBUG)
 stdout_handler.addFilter(warning_filter)
@@ -41,11 +42,11 @@ def startup():
     sections = config.sections()
     for section in sections:
         if section.startswith('SENSOR_'):
-            sample_interval = config.getfloat(section, 'sample_interval', fallback=0.01)
-            send_interval = config.getfloat(section, 'send_interval', fallback=0.1)
+            name = section.removeprefix('SENSOR_')
+            sample_interval = config.getfloat(section, 'sample_interval')
+            send_interval = config.getfloat(section, 'send_interval')
             mqtt_topic = config.get(section, 'mqtt_topic')
-            sensors.append(generators.Sensor(sample_interval, send_interval, mqtt_topic))
-            logging.info('Sensor %s spawned.' % section.removeprefix('SENSOR_'))
+            sensors.append(generators.Sensor(name, sample_interval, send_interval, mqtt_topic))
 
     for sensor in sensors:
         sensor.start()
