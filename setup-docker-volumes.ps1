e4edf11f009dc98adea4747949282786bc67d916
David Schmid <schmid.david@outlook.de>
2022-11-19 13:37:08 +0100

    changed docker compose bind mount setup

---
+++ setup-docker-volumes.ps1
@@ -0,0 +1,12 @@
+# This PowerShell script creates and sets up the Docker bind mount directories on the host machine as specified in the compose.yaml. 
+# The directories will be created in the folder docker-volumes/... insides this project folder and are ignored by source control (git).
+
+New-Item -Path './docker-volumes/chronograf' -ItemType Directory
+New-Item -Path './docker-volumes/influxdb/config' -ItemType Directory
+New-Item -Path './docker-volumes/telegraf/config' -ItemType Directory
+New-Item -Path './docker-volumes/mosquitto/config/' -ItemType Directory
+
+Copy-Item -Path './components/chronograf/chronograf-v1.db' -Destination './docker-volumes/chronograf/' -Force
+Copy-Item -Path './components/influxdb/influxdb.conf' -Destination './docker-volumes/influxdb/config/' -Force
+Copy-Item -Path './components/telegraf/telegraf.conf' -Destination './docker-volumes/telegraf/config/' -Force
+Copy-Item -Path './external/mosquitto/mosquitto.conf' -Destination './docker-volumes/mosquitto/config/' -Force
